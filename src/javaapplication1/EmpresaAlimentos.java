a350ee7ef7aa4ca3e17ee70397547d9b785cfb3f
p23 <p23@DESKTOP-IA25HD0>
2024-04-08 11:47:22 -0300

    3er

---
+++ src/javaapplication1/EmpresaAlimentos.java
@@ -0,0 +1,28 @@
+/*
+ prueba de comit
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author agg
+ */
+
+
+import java.util.Scanner; //libreria inputs
+
+public class EmpresaAlimentos {
+
+    
+    public static void main(String[] args) {
+        
+        
+        
+        System.out.println("-----------------------------------------");  //menu
+        System.out.println("----------Empresa de Alimentos-----------");
+        System.out.println("----------Ingrese la Información---------");
+        System.out.println("-----------------------------------------");
+        
+    }
+    
+}
