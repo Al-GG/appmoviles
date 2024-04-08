5bdcd7779cc38eb7525b56f68534a4f52a0b0d12
p23 <p23@DESKTOP-IA25HD0>
2024-04-08 11:17:12 -0300

    1er

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -5,15 +5,13 @@ package javaapplication1;
 
 /**
  *
- * @author p23
+ * @author agg
  */
 public class JavaApplication1 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
