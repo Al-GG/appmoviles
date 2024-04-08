8f16a4ecf5d0295d965311efede63d12a09811e7
p23 <p23@DESKTOP-IA25HD0>
2024-04-08 11:15:38 -0300

    1er

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,19 @@
+/*
+ prueba de comit
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author p23
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
