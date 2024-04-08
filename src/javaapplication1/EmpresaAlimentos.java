3f9d18961da0eef6d460d86b350fcbdbfd7a065d
p23 <p23@DESKTOP-IA25HD0>
2024-04-08 13:43:12 -0300

    mejorando la presentacion

--- src/javaapplication1/EmpresaAlimentos.java
+++ src/javaapplication1/EmpresaAlimentos.java
@@ -1,6 +1,3 @@
-/*
- prueba de comit
- */
 package javaapplication1;
 
 /**
@@ -23,62 +20,39 @@ public class EmpresaAlimentos {
         System.out.println("----------Ingrese la Información---------");
         System.out.println("-----------------------------------------");
         
+        
         System.out.println("----------Ingrese sus datos personales---");
-        
-        System.out.print("Ingrese su nombre:    "); // ingreso de datos usuario y print de datos
+        System.out.print("Ingrese su nombre:      "); // ingreso de datos usuario y print de datos
 	String nombre = scanner.nextLine();
-        //System.out.println(nombre);
-        
         System.out.print("Ingrese su apellido:    ");
 	String apellido = scanner.nextLine();
-        //System.out.println(apellido);
-                
-        System.out.print("Ingrese su edad:    ");
+        System.out.print("Ingrese su edad:        ");
 	int edad = scanner.nextInt();
-        //System.out.println(edad);
+      
 
         System.out.println("----------Ingrese datos del vehículo----"); //ingreso datos vehiculo
-        
-       
-        System.out.print("Ingrese la marca:    ");
+        System.out.print("Ingrese la marca:                    ");
         String marca = scanner.nextLine();
-         System.out.print("Ingrese el modelo:    ");
-            String modelo = scanner.nextLine();
-          System.out.print("Ingrese la cilindrada:    ");
-            String cilindrada = scanner.nextLine();
-           System.out.print("Ingrese el tipo de combustible:    ");
-             String combustible = scanner.nextLine();   
-           System.out.print("Ingrese la cantidad de pasajeros:    ");
-              String pasajeros = scanner.nextLine();
+        System.out.print("Ingrese el modelo:                   ");
+        String modelo = scanner.nextLine();
+        System.out.print("Ingrese la cilindrada:               ");
+        String cilindrada = scanner.nextLine();
+        System.out.print("Ingrese el tipo de combustible:      ");
+        String combustible = scanner.nextLine();   
+        System.out.print("Ingrese la cantidad de pasajeros:    ");
+        String pasajeros = scanner.nextLine();
               
-              
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-         System.out.println("Ingrese la compra:                  ");
+            
+         System.out.println("Ingrese la compra:                  "); //ingreso datos compra y distancia
          int compras = scanner.nextInt();
          System.out.println("Ingrese la distancia al despacho:   ");
          int distancia = scanner.nextInt();
          
                 
-                       //if else para calcular total
+             //if else para calcular total
             if (compras >= 50000) {
+              System.out.println("Datos del cliente: " +nombre+ " " +apellido+ " edad: " +edad);
+              System.out.println("Datos del vehículo : " +marca+ " " +modelo+ " " +cilindrada+ " "+combustible+" "+pasajeros);
               System.out.println("Despacho gratis");
               System.out.println("Total : " +compras);
               
